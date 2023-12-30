273452f1887f3572d1a5ef67f3081a3b5d3e796c
Muhammad Saim <muhammadsaim494@gmail.com>
2023-12-30 14:27:23 +0500

    format video urls & fix assest dependencies

--- src/VideoDownloader.php
+++ src/VideoDownloader.php
@@ -33,11 +33,45 @@ class VideoDownloader
 	}
 
 
-
+	/**
+	 *
+	 * fetch the download links from the page link
+	 * 
+	 * @param  string $link Youtube video url
+	 * @return array       return the formated array of links
+	 */
 	public function fetchDownloadLinks($link)
 	{
 		$links = $this->youtube_downloader->getDownloadLinks($link);
-		return $links->getAllFormats();
+		return json_encode($this->formatUrls($links->getCombinedFormats()));
 	}
+
+
+	/**
+	 *
+	 * format the urls
+	 *
+	 * @param array unformated array
+	 * 
+	 * 
+	 */
+
+	public function formatUrls($videos)
+	{
+
+		$urls = [];
+
+		foreach($videos as $video){
+
+			$urls[] = [
+				'quality' => $video->qualityLabel,
+				'url'     => $video->url
+			];
+
+		}
+
+		return $urls;
+	}
+
 
 }
--- tubecatcher.php
+++ tubecatcher.php
@@ -100,10 +100,10 @@ if (!class_exists('TubeCatcher')) {
             // css
             wp_enqueue_style('tubecatcher-bootstrap-style', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', [], null, 'all');
             wp_enqueue_style('tubecatcher-fontawesome-style', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css', [], null, 'all');
-            wp_enqueue_style('tubecatcher-style', plugins_url('assets/css/style.css', __FILE__), [], null, 'all');
+            wp_enqueue_style('tubecatcher-style', plugins_url('assets/css/style.css', __FILE__), ['tubecatcher-bootstrap-style', 'tubecatcher-fontawesome-style'], null, 'all');
             // js
             wp_enqueue_script('tubecatcher-bootstrap-script', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', [], true);
-            wp_enqueue_script('tubecatcher-script', plugins_url('assets/js/script.js', __FILE__), ['jquery'], true);
+            wp_enqueue_script('tubecatcher-script', plugins_url('assets/js/script.js', __FILE__), ['jquery', 'tubecatcher-bootstrap-script'], true);
 
             $video_data = (new VideoDownloader())->fetchDownloadLinks('https://www.youtube.com/watch?v=jADTdg-o8i0');
             var_dump($video_data);
